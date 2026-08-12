274d47bafcffe71c47268386ca8b3fd24f2c58b0
agusdYT90 <agusdyt90@gmail.com>
2026-08-12 16:14:40 -0300

    ""

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.VITE_API_KEY,
+    authDomain: process.env.VITE_AUTH_DOMAIN,
+    projectId: process.env.VITE_PROJECT_ID,
+    storageBucket: process.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_APP_ID,
+    measurementId: process.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
